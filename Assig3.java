6cc75160f3f683224cd695f34de0867a0353cd37
vineethsai9 <73883725+vineethsai9@users.noreply.github.com>
2024-03-01 17:00:07 -0500

    Add files via upload

---
+++ Assig3.java
@@ -0,0 +1,17 @@
+public class Assig3 {
+    public static void main(String[] args) {
+                 // FOR LOOPS//
+       // int i; for(i=0;i<=15;i++){System.out.println(i);}
+       // int i; for(i=0;i<=15;i++){ if(i%2==0){ System.out.println(i);}}
+       // int i; for(i=15;i>=1;i--){ if(i%2==1){ System.out.println(i);}}
+                 //WHILE-DO//
+        //  int i=0; while(i<=15){System.out.println(i); i++;}
+       // int i=0;while(i<=15){if(i%2==0){System.out.println(i);}i++;}
+       // int i=15; while(i>=1){if(i%2==1){System.out.println(i);}i--;}
+                  // DO-WHILE//
+       //int i=0; do{System.out.println(i);i++;}while(i<=15);
+       // int i=0;do {if(i%2==0){System.out.println(i);}i++;} while(i<=15);
+       // int i=15;do{if(i%2==1){System.out.println(i);} i--;} while (i>=0);
+
+    }
+}
